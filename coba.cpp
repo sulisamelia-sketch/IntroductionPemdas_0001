c986253bbc5ffe7ef63e337853225bf6c1eb35d8
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-02-23 20:29:43 +0700

    membuat program persegi

---
+++ coba.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int  main()
+{
+    int luas, sisi;
+
+    cout << "Masukan Sisi :";
+    cin >> sisi;
+
+    cout << "Masukan Sisi :";
+    cin >> sisi;
+
+    luas = sisi * sisi;
+
+    cout << "Hasilnya :" << luas;
+
+}
